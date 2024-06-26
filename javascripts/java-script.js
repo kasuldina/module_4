a91a7a94288e472cbc3bd90ab01e0dabd82eb54d
kasuldina <146091279+kasuldina@users.noreply.github.com>
2024-06-26 20:40:01 +0300

    back lol

--- javascripts/java-script.js
+++ javascripts/java-script.js
@@ -5,21 +5,21 @@ document.addEventListener("DOMContentLoaded", function() {
     // Это как Document ready, только для vanila js
 
 
-    // let lastScrollTop = 0;
-    // let element = document.querySelector('.secondHoloport');
-    // let moveAmount = 690; // Количество пикселей для сдвига
+    let lastScrollTop = 0;
+    let element = document.querySelector('.secondHoloport');
+    let moveAmount = 690; // Количество пикселей для сдвига
 
-    // window.addEventListener("scroll", function() {
-    //   let st = window.pageYOffset || document.documentElement.scrollTop;
-    //   if (st > lastScrollTop) {
-    //     // Прокрутка вниз
-    //     let currentRight = parseInt(window.getComputedStyle(element).getPropertyValue('right'));
-    //     element.style.right = (currentRight - moveAmount) + 'px'; // Изменение значения сдвига влево
-    //     // moveAmount += 10; 
-    //     // Увеличиваем количество пикселей для следующего сдвига
-    //   }
-    //   lastScrollTop = st <= 0 ? 0 : st; // Для обработки случаев, когда прокрутка достигла верха страницы
-    // });
+    window.addEventListener("scroll", function() {
+      let st = window.pageYOffset || document.documentElement.scrollTop;
+      if (st > lastScrollTop) {
+        // Прокрутка вниз
+        let currentRight = parseInt(window.getComputedStyle(element).getPropertyValue('right'));
+        element.style.right = (currentRight - moveAmount) + 'px'; // Изменение значения сдвига влево
+        // moveAmount += 10; 
+        // Увеличиваем количество пикселей для следующего сдвига
+      }
+      lastScrollTop = st <= 0 ? 0 : st; // Для обработки случаев, когда прокрутка достигла верха страницы
+    });
 
 
 
