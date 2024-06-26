8fe4d196173da7896be6347e57c89d1b7c54b57e
kasuldina <146091279+kasuldina@users.noreply.github.com>
2024-06-26 20:14:29 +0300

    Add files via upload

---
+++ javascripts/java-script.js
@@ -0,0 +1,28 @@
+//write here your js
+
+document.addEventListener("DOMContentLoaded", function() {
+    // используй эту функцию, для элементов которые должны быть выполнены после загрузки HTML
+    // Это как Document ready, только для vanila js
+
+
+    let lastScrollTop = 0;
+    let element = document.querySelector('.secondHoloport');
+    let moveAmount = 690; // Количество пикселей для сдвига
+
+    window.addEventListener("scroll", function() {
+      let st = window.pageYOffset || document.documentElement.scrollTop;
+      if (st > lastScrollTop) {
+        // Прокрутка вниз
+        let currentRight = parseInt(window.getComputedStyle(element).getPropertyValue('right'));
+        element.style.right = (currentRight - moveAmount) + 'px'; // Изменение значения сдвига влево
+        // moveAmount += 10; 
+        // Увеличиваем количество пикселей для следующего сдвига
+      }
+      lastScrollTop = st <= 0 ? 0 : st; // Для обработки случаев, когда прокрутка достигла верха страницы
+    });
+
+
+
+
+
+});
